c7a6d5c473c2f06ce137eaa08330f0005c2b7ea1
fadi <fadi.alset.94@gmail.com>
2020-12-08 14:45:56 +0100

    Finished the project and editedd the exercise 1

--- Week2/js-exercises/ex1-oddOnesOut.js
+++ Week2/js-exercises/ex1-oddOnesOut.js
@@ -7,24 +7,27 @@
  The function should still behave the same.
 
  */
-// function doubleEvenNumbers(numbers) {
-//   const newNumbers = [];
+function doubleEvenNumbers(numbers) {
+ const oddOnesOut = numbers.filter(number => {
+  return number % 2 ===0;
+});
+const newNumbers = oddOnesOut.map(number => {
+  return number * 2;
+})
+return newNumbers;
+}
+
+const myNumbers = [1, 2, 3, 4];
+console.log(doubleEvenNumbers(myNumbers)); // Logs "[4, 8]" to the console
+
+
+
+
+
+//  const newNumbers = [];
 //   for (let i = 0; i < numbers.length; i++) {
 //     if (numbers[i] % 2 === 0) {
 //       newNumbers.push(numbers[i] * 2);
 //     }
 //   }
 //   return newNumbers;
-// }
-
-// const myNumbers = [1, 2, 3, 4];
-// console.log(doubleEvenNumbers(myNumbers)); // Logs "[4, 8]" to the console
-
-const myNumbers = [1,2,3,4];
-const oddOnesOut = myNumbers.filter(number => {
-  return number % 2 ===0;
-});
-const newNumbers = oddOnesOut.map(number => {
-  return number * 2;
-})
-console.log(newNumbers);
